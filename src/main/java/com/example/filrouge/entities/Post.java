fdeb1a477a1959d9bbcb62eddbd54d2750cf84a6
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2025-11-06 11:12:29 +0100

    Projet créé, dépendances nécessaires importées et structure du projet créée

---
+++ src/main/java/com/example/filrouge/entities/Post.java
@@ -0,0 +1,17 @@
+package com.example.filrouge.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+}
